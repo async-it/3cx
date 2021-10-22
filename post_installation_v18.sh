7773001d675aadf3b20707aaca7760dd86905f6c
Async IT Sàrl <70369976+async-it@users.noreply.github.com>
2021-10-22 13:58:09 +0200

    Update post_installation_v18.sh

--- post_installation_v18.sh
+++ post_installation_v18.sh
@@ -9,6 +9,5 @@ wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key a
 echo "deb http://downloads-global.3cx.com/downloads/debian buster main" | tee /etc/apt/sources.list.d/3cxpbx.list
 echo "deb http://downloads-global.3cx.com/downloads/debian buster-testing main" | tee /etc/apt/sources.list.d/3cxpbx-testing.list
 apt update -y
-apt install -y net-tools dphys-swapfile snmpd
 username=$(getent passwd "1000" | cut -d: -f1)
 echo "$username   ALL=(ALL:ALL) ALL" >> /etc/sudoers
