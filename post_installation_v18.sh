7ed3bac41c5c4266834702df0b4a4fadb5790a96
Async IT Sàrl <70369976+async-it@users.noreply.github.com>
2022-06-01 15:51:59 +0200

    Update post_installation_v18.sh

--- post_installation_v18.sh
+++ post_installation_v18.sh
@@ -12,7 +12,13 @@ echo "$username   ALL=(ALL:ALL) ALL" >> /etc/sudoers
 # Running 3CX official post-installation script:
 # wget -O /tmp/3cxpostinstall.sh http://downloads.3cx.com/downloads/debian9iso/post-install_10.4.0.txt
 wget -O /tmp/3cxpostinstall.sh http://downloads-global.3cx.com/downloads/debian10iso/post-install_10.11.0_0cf5fcb.txt
-echo "apt update -y && apt upgrade -y" >> /tmp/3cxpostinstall.sh
+echo "apt update -y && apt upgrade -y
+dd if=/dev/zero of=/swap bs=1024 count=1048576
+chmod 600 /swapfile
+mkswap /swap
+swapon /swap
+echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+" >> /tmp/3cxpostinstall.sh
 chmod +x /tmp/3cxpostinstall.sh
 bash /tmp/3cxpostinstall.sh
 
