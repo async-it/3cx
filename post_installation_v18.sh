10ce6cca9828f8996b4df0583d1d268b0a5ad657
Async IT Sàrl <70369976+async-it@users.noreply.github.com>
2022-06-01 14:16:28 +0200

    Update post_installation_v18.sh

--- post_installation_v18.sh
+++ post_installation_v18.sh
@@ -10,7 +10,8 @@ username=$(getent passwd "1000" | cut -d: -f1)
 echo "$username   ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
 # Running 3CX official post-installation script:
-wget -O /tmp/3cxpostinstall.sh http://downloads.3cx.com/downloads/debian9iso/post-install_10.4.0.txt
+# wget -O /tmp/3cxpostinstall.sh http://downloads.3cx.com/downloads/debian9iso/post-install_10.4.0.txt
+wget -O /tmp/3cxpostinstall.sh http://downloads-global.3cx.com/downloads/debian10iso/preseed_10.11.0_0cf5fcb.txt
 chmod +x /tmp/3cxpostinstall.sh
 bash /tmp/3cxpostinstall.sh
 apt update -y
